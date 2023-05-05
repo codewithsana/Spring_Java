3480cee3134136e9b89f83adfd5181f567ca23af
codewithsana <122785459+codewithsana@users.noreply.github.com>
2023-05-05 18:58:32 +0530

    Add files via upload

---
+++ SpringBootBatch4119Ex1-3/src/main/java/com/spring/demo/SpringBootBatch4119Ex13Application.java
@@ -0,0 +1,13 @@
+package com.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatch4119Ex13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatch4119Ex13Application.class, args);
+	}
+
+}
